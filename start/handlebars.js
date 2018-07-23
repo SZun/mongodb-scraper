3956cb331d9a6ff6f110912904a8940ed0c0d3fa
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-23 12:19:37 -0500

    refactioring start files

--- start/handlebars.js
+++ start/handlebars.js
@@ -1,7 +1,7 @@
 const exphbs = require('express-handlebars');
 const path = require('path');
 
-module.exports = function(app, express) {
+module.exports = (app, express) => {
   app.use(express.static(path.join(__dirname, '/public')));
   app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
   app.set('view engine', 'handlebars');
