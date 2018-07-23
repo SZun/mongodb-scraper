37338ed9bc11c7c58c45530c0ad276a3841f0923
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-23 01:23:07 -0500

    Adding Bootstrap

--- start/handlebars.js
+++ start/handlebars.js
@@ -1,7 +1,8 @@
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 module.exports = function(app, express) {
-  app.use(express.static('public'));
+  app.use(express.static(path.join(__dirname, '/public')));
   app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
   app.set('view engine', 'handlebars');
 };
