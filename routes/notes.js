99c36dba191962b76254da4f381327c1f06a44e6
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-24 01:04:05 -0500

    refactoring notes route

--- routes/notes.js
+++ routes/notes.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Note = require('../models/note');
-const Article = require('../models/article')
+const Article = require('../models/article');
 
 router.post('/', async (req, res) => {
   try {
     const note = await new Note({
       note: req.body.note
-    })
-    note => {
-      const populate = async (note) => await Article.findByIdAndUpdate(
-        {_id: req.body.key},
-        {$push: {notes: note._id}}, 
-        {new: true})
-      const populatedNote = await populate(note)
+    });
+    async () => {
+      const populatedNote = await Article.findByIdAndUpdate(
+        { _id: req.body.id },
+        { $push: { notes: note._id } },
+        { new: true }
+      );
       res.send(populatedNote);
-    }
-  }
-  catch (err) {
+    };
+  } catch (err) {
     console.log(`Error: ${err.message}`);
   }
 });
