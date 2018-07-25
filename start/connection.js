18de1c9ff051fbf92b40d9c7bd268deee101e34d
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-25 15:41:47 -0500

    Updating connection

--- start/connection.js
+++ start/connection.js
@@ -2,12 +2,13 @@ module.exports = async mongoose => {
   try {
     if (process.env.NODE_ENV === 'production') {
       const connection = await mongoose.connect(process.env.MONGODB_URI);
+      console.log(`Connected to MongoDB PROD`);
     } else {
       const connection = await mongoose.connect(
         'mongodb://localhost/mongodb_scraper_hw'
       );
+      console.log(`Connected to MongoDB DEV`);
     }
-    console.log(`Connected to MongoDB`);
   } catch (err) {
     err => console.log(`Error: ${err.message}`);
   }
